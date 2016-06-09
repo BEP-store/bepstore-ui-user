d22ea58fd3e6bcc2297fc2178eea6e3c5c955f81
Bart Heemskerk <de.uil.met.een@gmail.com>
2016-06-09 15:57:57 +0200

    removed styles dependency, set show to component

--- addon/components/profile/show.js
+++ addon/components/profile/show.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import layout from 'bepstore-user/templates/components/profile/show';
 
-export default Ember.Route.extend({
+export default Ember.Component.extend({
   layout
 });
